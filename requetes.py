72627392250a5a7959ce0d2e69db65a709906d9d
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-04-16 14:27:12 +0200

    Ajout de la durée d'execution

--- requetes.py
+++ requetes.py
@@ -1,25 +1,29 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+from datetime import datetime
+
+start_time = datetime.now()
+
 def from_json(nom_fichier):
     liste_csv = []
     try:
         file = open(nom_fichier, "r", encoding="utf8")
         for line in file:
             line = line.split("\n")[0]
-            liste_csv.append(eval(line))
+            liste_csv.append(formattage(eval(line)))
         file.close()
     except Exception as err:
         print(err)
     return liste_csv
 
 
-def formattage(data):
-    for film in data:
-        acteurs = film['cast']
-        for i in range(len(acteurs)):
-            acteurs[i] = acteurs[i].strip('[]')
-    return data
+def formattage(film):
+    acteurs = film['cast']
+    for i in range(len(acteurs)):
+        acteur = acteurs[i]
+        acteur = acteur.strip('[]')
+    return film
 
 
 def genere_graph(data):
@@ -31,9 +35,13 @@ def genere_graph(data):
                 G.add_edge(acteurs[i], acteurs[j])
     return G
 
-
 liste_json = from_json("extrait.txt")
-liste_json = formattage(liste_json)
 G = genere_graph(liste_json)
-nx.draw(G, with_labels=True)
-plt.show()
+
+
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
+
+
+#nx.draw(G, with_labels=True)
+#plt.show()
