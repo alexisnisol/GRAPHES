6e79a5c8385541156e1e971a3e53c7eab0957104
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-05-07 14:07:06 +0200

    Ajout de la méthode collaborateurs_proches

--- requetes.py
+++ requetes.py
@@ -71,8 +71,27 @@ def collaborateurs_communs(G,u,v):
 
 # Q3
 def collaborateurs_proches(G,u,k):
-#    nx.single_source_shortest_path_length(G,u,k)
-    ...
+    """Fonction renvoyant l'ensemble des acteurs à distance au plus k de l'acteur u dans le graphe G. La fonction renvoie None si u est absent du graphe.
+    
+    Parametres:
+        G: le graphe
+        u: le sommet de départ
+        k: la distance depuis u
+    """
+    if u not in G.nodes:
+        print(u,"est un illustre inconnu")
+        return None
+    collaborateurs = set()
+    collaborateurs.add(u)
+    print(collaborateurs)
+    for i in range(k):
+        collaborateurs_directs = set()
+        for c in collaborateurs:
+            for voisin in G.adj[c]:
+                if voisin not in collaborateurs:
+                    collaborateurs_directs.add(voisin)
+        collaborateurs = collaborateurs.union(collaborateurs_directs)
+    return collaborateurs
 
 def est_proche(G,u,v,k=1):
     ...
